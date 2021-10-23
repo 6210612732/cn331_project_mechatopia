fe3d5cb1fa2283359090ba7aa145d6fcbfc19a0a
Pornchai Phumphinyo <6210612732@student.tu.ac.th>
2021-10-23 11:02:03 +0700

    add some url

--- mechatopia/activate_env.py
+++ mechatopia/activate_env.py
@@ -1,8 +1,11 @@
 import os
 from pathlib import Path
-print("aa")
-pp = "C:\\Users\\Administrator\\Desktop\\cn331_project_mechatopia\\.venv_project\\Scripts\\activate";
-print(pp)
-#os.system('cmd /k "C:\\Users\\Administrator\\Desktop\\cn331_project_mechatopia\\.venv_project\\Scripts\\activate"')
+print("\n###   activate venv!   ###")
+pp = os.getcwd()+"\\..\\.venv_project\\Scripts\\activate";
+#print(pp)
 os.system('cmd /k ' + pp)
-#os.system("cmd /k cd C:\Users\Administrator\Desktop\cn331_project_mechatopia\.venv_project\Script\activate" )
+
+
+#print('getcwd:      ', os.getcwd())
+#print('__file__:    ', __file__)
+#os.system('cmd /k "C:\\Users\\Administrator\\Desktop\\cn331_project_mechatopia\\.venv_project\\Scripts\\activate"')
